108113684831ff4ea99c14105952aaf064198fa2
djwbhr <sergey.evseev62@gmail.com>
2025-04-23 14:09:48 +0900

    build fix and fcm done

--- android/app/src/main/java/com/dtc/mobile/MainActivity.kt
+++ android/app/src/main/java/com/dtc/mobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.dtcmobile
+package com.dtc.mobile
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/dtc/mobile/MainApplication.kt
+++ android/app/src/main/java/com/dtc/mobile/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.dtcmobile
+package com.dtc.mobile
 
 import android.app.Application
 import android.content.res.Configuration
